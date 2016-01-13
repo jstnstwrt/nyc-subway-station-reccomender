4ad1565c51dc5875296ff03f4e6e74ccf1487985
Justin Stewart <justinstewart.la@gmail.com>
2016-01-13 00:17:59 -0500

    modified protocol so that it handels nan

--- get_zip.py
+++ get_zip.py
@@ -1,3 +1,4 @@
+import math 
 import pandas as pd
 import numpy as np
 from geopy.geocoders import Nominatim
@@ -6,10 +7,18 @@ df = pd.read_csv('mta_geo_tags.csv',header = None)
 mta = np.array(df)
 geolocator = Nominatim()
 
-mta = mta[:2]
+mta = mta[-5:]
+mta_zips = np.empty((0,8))
 for station in mta:
-	geo = station[5],station[6]
-	location = geolocator.reverse(geo)
-	zipcode = str(location.raw['address']['postcode'])
-	print geo , zipcode
+	if math.isnan(station[5]):
+		station = np.append(station,float('nan'))
+	else:
+		print 'in else'
+		geo = station[5],station[6]
+		location = geolocator.reverse(geo)
+		zipcode = str(location.raw['address']['postcode'])
+		station = np.append(station,zipcode)
+	mta_zips = np.append(mta_zips,[station],axis = 0)
 
+
+print mta_zips
