7f4cdc3423357ce45c0b1d8b32f5587545d5e25b
Justin Stewart <justinstewart.la@gmail.com>
2016-01-13 20:50:09 -0500

    ranking real data

--- analysis.py
+++ analysis.py
@@ -5,12 +5,15 @@ df = pd.DataFrame([['a',1,6],['b',3,4],['c',5,2]])
 df.columns = ['z','t','w']
 
 
+da = pd.read_csv('wealth_startups_mta_entrances.csv')
+da = da.drop_duplicates(subset = 'zip')
+
 def rank_zips(alpha=1):
-	t = df['t']
-	w = df['w']
+	t = da['startups_index']
+	w = da['income_index']
 	r = alpha*t + (1-alpha)*w
-	df['r'] = r
-	print df.sort(['r'])['z']
+	da['r'] = r
+	print da.sort(['r'],ascending = 0)[['zip','r']][:10]
 
 rank_zips(1)
 
