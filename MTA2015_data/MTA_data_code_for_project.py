5bf26a3cc79e3b9fb6317649c8a9d6229f5dae5f
Ken <kennjamesmyers@gmail.com>
2016-01-14 12:40:04 -0500

    fixed formatting of times

--- MTA2015_data/MTA_data_code_for_project.py
+++ MTA2015_data/MTA_data_code_for_project.py
@@ -69,7 +69,7 @@ def dframes_of_weeks_and_hours(*args):
                             }
         }'''
     
-    full_dict = defaultdict(lambda : defaultdict(lambda :defaultdict(int)))
+    full_dict = defaultdict(lambda : defaultdict(lambda : defaultdict(int)))
     cols = ["Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"]
     
     for file in args:
@@ -83,7 +83,12 @@ def dframes_of_weeks_and_hours(*args):
             for item in v:
                 day_string = item[0].strftime("%A")
                 time = item[0].time()
-                full_dict[newKey][day_string][time] += item[1] 
+                round_time = datetime.datetime.strptime(
+                            str(int(time.hour / 4) * 4).zfill(2), "%H")
+                round_time = round_time.time()
+                #print(type(round_time))
+                #break
+                full_dict[newKey][day_string][round_time] += item[1] 
     
     
     df_dict = {turnstile: pd.DataFrame.from_dict(value) 
@@ -92,6 +97,7 @@ def dframes_of_weeks_and_hours(*args):
         df_dict[k] = v[cols]
         
     return df_dict
+
 
 summer_data = dframes_of_weeks_and_hours('turnstile_150530.csv',  'turnstile_150606.csv', 'turnstile_150613.csv', 'turnstile_150620.csv', 'turnstile_150627.csv', 'turnstile_150704.csv', 'turnstile_150711.csv', 'turnstile_150718.csv', 'turnstile_150725.csv', 'turnstile_150801.csv', 'turnstile_150801.csv', 'turnstile_150808.csv', 'turnstile_150815.csv', 'turnstile_150822.csv', 'turnstile_150829.csv', 'turnstile_150905.csv', 'turnstile_150912.csv')
 
