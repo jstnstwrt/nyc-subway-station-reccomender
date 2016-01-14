bc76ebde3b6f7220a50a4a1ed01bba2251462f67
Justin Stewart <justinstewart.la@gmail.com>
2016-01-13 20:20:40 -0500

    mocking up station recommenation fn

--- analysis.py
+++ analysis.py
@@ -1,8 +1,29 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('wealth_startups_mta_entrances.csv')
-df.columns = ['rank', 'zip', 'population', 'income', 
-			  'startups', 'national_rank', 'ca', 'unit', 
-			  'station', 'line']
+df = pd.DataFrame([[1,2],[3,4],[5,6]])
+df.columns = ['t','w']
+
+def rank_zips(alpha=1):
+	# for each zipcode
+	# compute mixture of tech and wealth
+	# ie, at + (1-a)w
+	# return ranked list of zips
+	pass
+
+def zip_to_station(zipcode):
+	# query data set for all staions in zipcode
+	# return list of stations
+	pass
+
+
+
+def best_stations(ranked_zips):
+
+	pass
+
+
+# constraints on team
+team_size = 10
+max_team_in_zip = 5
 
